28165bbf155f10224dd1faa56b29de69e682e6f4
PoKaraNya <zbunkarina@gmail.com>
2024-01-13 00:07:28 +0200

    initial

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo_src { get; set; }
+        public int price { get; set; }
+        public int count { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        //public string Category { get; set; }
+
+    }
+}
