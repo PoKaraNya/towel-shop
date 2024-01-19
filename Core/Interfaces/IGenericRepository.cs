51eae5fd16a4cb986ed0a4b8d47d3f56ef4da264
PoKaraNya <zbunkarina@gmail.com>
2024-01-19 14:03:05 +0200

    Add pagination

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.Specifications;
+﻿using Core.Specifications;
 
-namespace Core.Interfaces
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T>
 {
-    public interface IGenericRepository<T>
-    {
-        Task<T> GetByIdAsync(int id);
-        Task<IReadOnlyList<T>> ListAllAsync();
-        Task<T> GetEntityWithSpec(ISpecification<T> spec);
-        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
-    }
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T> GetEntityWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<int> CountAsync(ISpecification<T> spec);
 }
--- Core/Specifications/ProductsWithCategoriesSpecification.cs
+++ Core/Specifications/ProductsWithCategoriesSpecification.cs
@@ -1,16 +1,38 @@
-﻿using System.Linq.Expressions;
-using Core.Entities;
+﻿using Core.Entities;
 
 namespace Core.Specifications;
 
 public class ProductsWithCategoriesSpecification : BaseSpecification<Product>
 {
-    public ProductsWithCategoriesSpecification()
+    public ProductsWithCategoriesSpecification(ProductSpecParams productSpecParams)
+        : base(x =>
+            (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search))
+            &&
+            (!productSpecParams.CategoryId.HasValue || x.ProductCategoryId == productSpecParams.CategoryId)
+        )
     {
         AddInclude(x => x.ProductCategory);
+        AddOrderBy(x => x.Name);
+        ApplyPaging(productSpecParams.PageSize * (productSpecParams.PageIndex - 1), productSpecParams.PageSize);
+
+        if (!string.IsNullOrEmpty(productSpecParams.Sort))
+        {
+            switch (productSpecParams.Sort)
+            {
+                case "priceAsc":
+                    AddOrderBy(p => p.Price);
+                    break;
+                case "priceDesc":
+                    AddOrderByDescending(p => p.Price);
+                    break;
+                default:
+                    AddOrderBy(n => n.Name);
+                    break;
+            }
+        }
     }
 
-    public ProductsWithCategoriesSpecification(int id) 
+    public ProductsWithCategoriesSpecification(int id)
         : base(x => x.Id == id)
     {
         AddInclude(x => x.ProductCategory);
