9097c9587ecdbfa56280e3cd60b6c85180b031f3
PoKaraNya <zbunkarina@gmail.com>
2024-01-25 22:24:20 +0200

    nav-bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgxPopper } from 'angular-popper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
+    // NgxPopper,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
