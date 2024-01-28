392b5f402af6ac7723e73e55d7b0ed4c56572084
PoKaraNya <zbunkarina@gmail.com>
2024-01-28 13:29:05 +0200

    add sort

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -12,6 +12,13 @@ import {Category} from "../shared/models/category";
 export class ShopComponent implements OnInit {
   products: Product[] = [];
   categories: Category[] = [];
+  categoryIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to high', value: 'priceAsc'},
+    {name: 'Price: High to low', value: 'priceDesc'},
+  ];
 
   constructor(private shopService: ShopService) {
   }
@@ -22,7 +29,7 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts().subscribe({
+    this.shopService.getProducts(this.categoryIdSelected, this.sortSelected).subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error)
     })
@@ -30,10 +37,29 @@ export class ShopComponent implements OnInit {
 
   getCategories() {
     this.shopService.getCategories().subscribe({
-      next: response => this.categories = response,
+      next: response => this.categories = [
+        {
+          id: 0,
+          title: 'All',
+          description: 'All',
+          pictureUrl: 'All',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        ...response
+      ],
       error: error => console.log(error)
     })
   }
 
+  onCategorySelected(categoryId: number) {
+    this.categoryIdSelected = categoryId;
+    this.getProducts();
+  }
+
+  onSortSelected(event: any) {
+    this.sortSelected = event.target.value;
+    this.getProducts();
+  }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Pagination} from "../shared/models/pagination";
 import {Product} from "../shared/models/product";
 import {Category} from "../shared/models/category";
@@ -13,8 +13,13 @@ export class ShopService {
   constructor(private http: HttpClient) {
   }
 
-  getProducts() {
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products')
+  getProducts(categoryId?: number, sort?: string) {
+    let params = new HttpParams();
+
+    if (categoryId) params = params.append('CategoryId', categoryId);
+    if (sort) params = params.append('sort', sort);
+
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params})
   }
 
   getCategories() {
