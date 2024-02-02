d2866a19d7bd4a6f0021a51daf65d1f8dae5c7ec
PoKaraNya <zbunkarina@gmail.com>
2024-02-02 12:47:53 +0200

    add CarouselModule

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,5 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import {Injectable} from "@angular/core";
+import {delay, finalize, Observable} from "rxjs";
+import {BusyService} from "../services/busy.service";
 
-export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private busyService: BusyService) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+   this.busyService.busy();
+    return next.handle(request).pipe(
+      delay(1000),
+      finalize( () => this.busyService.idle())
+    );
+  }
+}
