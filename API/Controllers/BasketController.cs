f3ed3214f8d161bf8d49b1291539a68e07392495
PoKaraNya <zbunkarina@gmail.com>
2024-02-17 15:28:26 +0200

    add redis and basket implementation on backend

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BasketController(IBasketRepository basketRepository) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<CustomerBasket>> GetBasketId(string id)
+    {
+        var basket = await basketRepository.GetBasketAsync(id);
+        return Ok(basket ?? new CustomerBasket(id));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+    {
+        var updatedBasked = await basketRepository.UpdateBasketAsync(basket);
+        return Ok(updatedBasked);
+    }
+
+    [HttpDelete]
+    public async Task DeleteBasketAsync(string id)
+    {
+        await basketRepository.DeleteBasketAsync(id);
+    }
+}
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -9,21 +9,22 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
   constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) {
   }
-    ngOnInit(): void {
-       this.loadProduct();
-    }
 
-    loadProduct(){
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
-    if(id) this.shopService.getProduct(+id).subscribe({
+    if (id) this.shopService.getProduct(+id).subscribe({
       next: product => this.product = product,
       error: error => console.log(error)
     });
-    }
+  }
 
 }
